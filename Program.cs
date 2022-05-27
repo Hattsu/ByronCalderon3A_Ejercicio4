21fe5ace64f811f89cba64fc061aa356f85bc3a7
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-05-26 23:15:32 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ByronCalderon3A_Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numeros numeros = new Numeros(51);
+            numeros.Calcular();
+        }
+    }
+}
